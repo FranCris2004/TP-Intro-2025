bd7fa8a023774c76ed833d2e1992b7ce09fd4440
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-26 16:13:35 -0300

    se ordenaron las funciones de usuario_service

--- backend/src/services/usuario_service.js
+++ backend/src/services/usuario_service.js
@@ -1,6 +1,47 @@
 import conn from "../services/db_connection.js";
 import Usuario from "../models/usuario.js";
 
+//
+// Create
+//
+
+async function createUsuario(nombre, contrasenia, email, fecha_de_nacimiento) {
+  try {
+    if (!nombre || nombre == "")
+      throw new Error("El nombre debe ser un string no vacio");
+
+    if (!contrasenia || contrasenia == "")
+      throw new Error("La contrasenia debe ser un string no vacio");
+
+    const res = await conn.query(
+      `
+      INSERT INTO usuario (nombre, contrasenia, email, fecha_de_nacimiento)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *
+      `,
+      [nombre, contrasenia, email, fecha_de_nacimiento]
+    );
+
+    if (res.rowCount === 0)
+      throw new Error("Fallo al insertar el nuevo usuario a la base de datos");
+
+    return new Usuario(
+      res.rows[0].id,
+      res.rows[0].nombre,
+      res.rows[0].email,
+      res.rows[0].fecha_registro,
+      res.rows[0].fecha_de_nacimiento
+    );
+  } catch (error) {
+    console.error("Error en createUsuario:", error);
+    throw error;
+  }
+}
+
+//
+// Read
+//
+
 async function getAllUsuarios() {
   try {
     const res = await conn.query("SELECT * FROM usuario");
@@ -43,38 +84,9 @@ async function getUsuarioById(id, contrasenia) {
   }
 }
 
-async function createUsuario(nombre, contrasenia, email, fecha_de_nacimiento) {
-  try {
-    if (!nombre || nombre == "")
-      throw new Error("El nombre debe ser un string no vacio");
-
-    if (!contrasenia || contrasenia == "")
-      throw new Error("La contrasenia debe ser un string no vacio");
-
-    const res = await conn.query(
-      `
-      INSERT INTO usuario (nombre, contrasenia, email, fecha_de_nacimiento)
-      VALUES ($1, $2, $3, $4)
-      RETURNING *
-      `,
-      [nombre, contrasenia, email, fecha_de_nacimiento]
-    );
-
-    if (res.rowCount === 0)
-      throw new Error("Fallo al insertar el nuevo usuario a la base de datos");
-
-    return new Usuario(
-      res.rows[0].id,
-      res.rows[0].nombre,
-      res.rows[0].email,
-      res.rows[0].fecha_registro,
-      res.rows[0].fecha_de_nacimiento
-    );
-  } catch (error) {
-    console.error("Error en createUsuario:", error);
-    throw error;
-  }
-}
+//
+// Update
+//
 
 async function validateIdUsuario(id) {
   return (
@@ -97,7 +109,10 @@ async function updateUsuarioById(
       throw new Error("Usuario no encontrado");
 
     if (nombre)
-      await conn.query("UPDATE usuario SET nombre = $2 WHERE id = $1", [id, nombre]);
+      await conn.query("UPDATE usuario SET nombre = $2 WHERE id = $1", [
+        id,
+        nombre,
+      ]);
 
     if (contrasenia)
       await conn.query("UPDATE usuario SET contrasenia= $2 WHERE id = $1", [
@@ -106,18 +121,25 @@ async function updateUsuarioById(
       ]);
 
     if (email)
-      await conn.query("UPDATE usuario SET email = $2 WHERE id = $1", [id, email]);
+      await conn.query("UPDATE usuario SET email = $2 WHERE id = $1", [
+        id,
+        email,
+      ]);
 
     if (fecha_de_nacimiento)
-      await conn.query("UPDATE usuario SET fecha_nacimiento = $2 WHERE id = $1", [
-        id,
-        fecha_nacimiento,
-      ]);
+      await conn.query(
+        "UPDATE usuario SET fecha_nacimiento = $2 WHERE id = $1",
+        [id, fecha_nacimiento]
+      );
   } catch (error) {
     console.error("Error en updateUsuarioById:", error);
     throw error;
   }
 }
+
+//
+// Delete
+//
 
 async function deleteUsuarioById(id) {
   try {
